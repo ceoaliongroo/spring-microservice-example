5f70da4e9816ddf7d45287a2c8544c8d5ea688d7
Carlos Mantilla <ceo@aliongroo.com>
2016-09-22 19:31:12 +0300

    chore(all): update project

--- secure-micro/src/main/java/com/mantilla/controllers/SecureDemo.java
+++ secure-micro/src/main/java/com/mantilla/controllers/SecureDemo.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecureDemo {
 
-    @RequestMapping("/")
+    @RequestMapping("/plus")
     public @ResponseBody String root() {
         return "{\"value\": \"Secure Demo\"}";
     }
